5ede2de31eaff1e2701df9add5155fc97e6dd8bb
tohitsugu <tohitsugu@gmail.com>
2017-03-09 13:47:38 -0800

    added monitor script
    watches directory for automatic submission to cuckoo. works with monitor-client for automatic submission of remote folders.

---
+++ utils/monitor.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python                                                                                                                                
+
+import submit
+import pyinotify                                                                    
+import fnmatch                                                                      
+import os
+
+wm = pyinotify.WatchManager()  # Watch Manager
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
+
+remotehost = "10.30.0.87"
+remotefile = "/home/tohitsugu/cuckoo/testing/"
+
+def suffix_filter(fn):                                                              
+    suffixes = ["*.clean", "*.clean.*"]                                                                                                                
+    for suffix in suffixes:                                                         
+        if fnmatch.fnmatch(fn, suffix):                                             
+            return False                                                            
+    return True                                                                     
+
+class EventHandler(pyinotify.ProcessEvent):                                         
+    def process_IN_CREATE(self, event):                                             
+        if  suffix_filter(event.name):                                           
+            print "Creating:", event.pathname                                       
+   	    localfile = event.pathname
+
+
+            print "Submitting:", event.pathname	
+           # os.system('scp "%s" tohitsugu@"%s:%s"' % (localfile, remotehost, remotefile) )
+            os.system("python submit.py "+event.pathname) 
+
+
+
+    def process_IN_DELETE(self, event):                                             
+        if  suffix_filter(event.name):                                           
+            print "Removing:", event.pathname                                       
+
+    def process_IN_MODIFY(self, event):                                             
+        if  suffix_filter(event.name):                                           
+            print "Modifing:", event.pathname                                       
+
+    def process_default(self, event):                                               
+        print "Default:", event.pathname
+
+handler = EventHandler()
+notifier = pyinotify.Notifier(wm, handler)
+wdd = wm.add_watch("./", mask, rec=True)
+
+notifier.loop()
+
+
